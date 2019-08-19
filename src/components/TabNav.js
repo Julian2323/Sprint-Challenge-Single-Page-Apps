fc9a43893ceb24bd79167a8cf1f4ebbced2437f5
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-08-19 17:29:32 -0500

    MVP COMPLETED

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,40 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+
+
+const Container = styled.div `
+    border: 2px solid black;
+    display: flex;
+    justify-content: space-evenly;
+`
+
+const StyledLink = styled.link `
+    color: black;
+`
+
+
+
+
 
 // TODO: Add missing tabs below
 export default function TabNav() {
+    return (
+        <>
 
+            <Container className="nav-links">
+                <NavLink className="some-nav" to="/character">
+                Characters
+                </NavLink>
+                <NavLink className="some-nav" to="/episode">
+                Episodes
+                </NavLink>
+                <NavLink className="some-nav" to="/location">
+                Locations
+                </NavLink>
+            </Container>
+
+        </>
+    )
 };
